bf964c34e656664802e0bf84c1cd04f7c1ffcc29
daryl2158 <daryl2158@hotmail.com>
2015-09-27 14:09:36 +0800

    Updated database
    Added bills and template DBs, with accompanying diagrams and master EER
    diagram

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS billDB;
+DROP DATABASE IF EXISTS templateDB;
 DROP DATABASE IF EXISTS userDB;
 
 CREATE DATABASE userDB;
@@ -22,7 +24,7 @@ CREATE table userDB.userAccts
     activationCode varchar(100),
     
     PRIMARY KEY (userName),
-    FOREIGN KEY (userName) REFERENCES Users(userName)
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -33,7 +35,7 @@ CREATE table userDB.userProfs
     realName varchar(100) NOT NULL,
     
     PRIMARY KEY (userName),
-    FOREIGN KEY (userName) REFERENCES Users(userName)
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -44,7 +46,7 @@ CREATE table userDB.emails
     userEmail varchar(255) NOT NULL,
     
     PRIMARY KEY (userName, userEmail),
-    FOREIGN KEY (userName) REFERENCES Users(userName)
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -59,7 +61,7 @@ CREATE table userDB.bankAccts
     acctBalance INTEGER,
     
     PRIMARY KEY (bankAcctID),
-    FOREIGN KEY (userName) REFERENCES Users(userName)
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -70,11 +72,96 @@ CREATE table userDB.friends
     friendName varchar(100) NOT NULL,
     
     PRIMARY KEY (userName, friendName),
-    FOREIGN KEY (userName) REFERENCES Users(userName)
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
     
-    FOREIGN KEY (friendName) REFERENCES Users(userName)
+    FOREIGN KEY (friendName) REFERENCES userDB.Users(userName)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
+
+CREATE DATABASE templateDB;
+
+USE templateDB;
+
+CREATE TABLE templateDB.templates
+(
+	templateID INTEGER AUTO_INCREMENT NOT NULL,
+    billOrg varchar (100),
+    creatorName varchar (100),
+    fileImgPath varchar (255),
+    fileCoordinatePath varchar (255),
+    extraPayableMap varchar (100),
+    
+    PRIMARY KEY (templateID),
+    FOREIGN KEY (creatorName) REFERENCES userDB.users(userName)
+);
+
+CREATE DATABASE billDB;
+
+USE billDB;
+
+CREATE table billDB.bills
+(
+	billID INTEGER AUTO_INCREMENT NOT NULL,
+    submittedTimeStamp timestamp NOT NULL,
+    billFilePath varchar (255) NOT NULL UNIQUE,
+    userName varchar(100) NOT NULL,
+    
+    PRIMARY KEY (billID),
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE table billDB.sharing
+(
+	billID INTEGER NOT NULL,
+    userName varchar(100) NOT NULL,
+    permissionType varchar(20) NOT NULL,
+    
+    PRIMARY KEY (billID,userName),
+    FOREIGN KEY (userName) REFERENCES userDB.Users(userName)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    
+    FOREIGN KEY (billID) REFERENCES billDB.bills(billID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    
+);
+
+CREATE table billDB.billDetails
+(
+	billID INTEGER NOT NULL UNIQUE,
+    templateID INTEGER,
+    billSentDate date,
+    billDueDate date,
+    billIsComplete boolean NOT NULL,
+    billIsCopy boolean NOT NULL,
+	billCompleteDateTime datetime,
+    billModifiedTimeStamp timestamp,
+    amtPayable decimal(19,4),
+    amtPaid decimal (19,4),
+    amtBalance decimal (19,4),
+    
+    -- Optional fields for other payments, e.g breakdowns
+    extraPayable1 decimal(19,4),
+    extraPaid1 decimal (19,4),
+    extraBalance decimal (19,4),
+    -- End of optional fields
+    
+    tagString varchar (255),
+    flags varchar (255),
+    
+    PRIMARY KEY (billID),
+    FOREIGN KEY (billID) REFERENCES billDB.bills(billID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    
+    FOREIGN KEY (templateID) REFERENCES templateDB.templates(templateID)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
