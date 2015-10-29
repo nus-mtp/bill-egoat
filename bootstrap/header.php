a1e86c8bbca535d95467b8b1c39b67e210f88629
daryl2158 <daryl2158@hotmail.com>
2015-10-29 18:06:50 +0800

    header

---
+++ bootstrap/header.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="favicons/android-icon-192x192.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+    <link rel="icon" href="favicons/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicons/favicon.ico" type="image/x-icon" />
+    <link rel="manifest" href="favicons/manifest.json" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png" />
+    <meta name="theme-color" content="#ffffff" />
+
+	<meta name="description" content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
+
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Bill.eGoat" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="https://www.billegoat.gq/" />
+	<meta property="og:image" content="https://www.billegoat.gq/images/landing/logo.png" />
+	<meta property="og:description" content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
+
+	<link href='https://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	
+    <title>Bill.eGoat</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    
+  </head>
+  <body>
+  <link href="css/header.css" rel="stylesheet" />
+  <!--Navbar-->
+  <div class="row">
+    <nav class="navbar navbar-default">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+      <!-- Responsive nav bar settings -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+      </button> 
+      <a class="navbar-brand" href="https://www.billegoat.gq">
+        <img class="img-responsive" style="max-height:37px" src="images/landing/logo.png" alt="Logo" />
+      </a></div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <!-- Nav bar content -->
+        <ul class="nav navbar-nav navbar-right">
+		<!--
+          <li>
+            <a href="#" id="login" data-toggle="modal" data-target="#myModal">Log In</a>
+          </li>
+          <li>
+            <a href="#signupForm">
+              <button class="btn-primary btn-lg" type="button" id="signup">Sign Up</button>
+            </a>
+          </li>-->
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </nav>
+  </div>
+  <!--End of Navbar-->
+  
+    <!--Navbar 2-->
+  <div class="row">
+    <nav class="navbar navbar-default" id="subnav">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+      <!-- Responsive nav bar settings -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+      </button> 
+      <p>This is the second bar</p>
+	  
+	  </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <!-- Nav bar content -->
+        <ul class="nav navbar-nav navbar-right" id="subnavbar">
+		<!--
+          <li>
+            <a href="#" id="login" data-toggle="modal" data-target="#myModal">Log In</a>
+          </li>
+          <li>
+            <a href="#signupForm">
+              <button class="btn-primary btn-lg" type="button" id="signup">Sign Up</button>
+            </a>
+          </li>-->
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </nav>
+  </div>
+  <!--End of Navbar 2-->
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+   
+  <script src="js/bootstrap.min.js"></script> 
+  <script src="js/validator.min.js"></script></div></body>
+</html>
