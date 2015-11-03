459ac2be0c3e0f9b28729f8cf94fbbe790d9699d
daryl2158@hotmail.com <daryl2158@hotmail.com>
2015-11-04 00:17:46 +0800

    updated header

--- bootstrap/header.php
+++ bootstrap/header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
@@ -22,97 +24,128 @@
     <meta name="msapplication-TileColor" content="#ffffff" />
     <meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png" />
     <meta name="theme-color" content="#ffffff" />
-
-	<meta name="description" content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
-
-	<!-- Open Graph data -->
-	<meta property="og:title" content="Bill.eGoat" />
-	<meta property="og:type" content="article" />
-	<meta property="og:url" content="https://www.billegoat.gq/" />
-	<meta property="og:image" content="https://www.billegoat.gq/images/landing/logo.png" />
-	<meta property="og:description" content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
-
-	<link href='https://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
-	
+    <meta name="description"
+    content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
+    <!-- Open Graph data -->
+    <meta property="og:title" content="Bill.eGoat" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://www.billegoat.gq/" />
+    <meta property="og:image" content="https://www.billegoat.gq/images/landing/logo.png" />
+    <meta property="og:description"
+    content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
+    <link href='https://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css' />
     <title>Bill.eGoat</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" />
-    
   </head>
   <body>
   <link href="css/header.css" rel="stylesheet" />
   <!--Navbar-->
-  <div class="row">
+  <div class="row" id="first-row">
     <nav class="navbar navbar-default">
       <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-      <!-- Responsive nav bar settings -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
+      <div class="container">
+        <div class="navbar-header">
+        <!-- Responsive nav bar settings -->
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+		  <span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-      </button> 
-      <a class="navbar-brand" href="https://www.billegoat.gq">
-        <img class="img-responsive" style="max-height:37px" src="images/landing/logo.png" alt="Logo" />
-      </a></div>
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        </button> 
+
+        <a class="navbar-brand" href="https://www.billegoat.gq">
+          <img class="img-responsive" style="max-height:40px" src="images/landing/logo.png" alt="Logo" />
+        </a></div>
+		
+        <div class="col-md-4" id="searchbar">
+          <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search for Bills" /> 
+          <span class="input-group-btn">
+            <button class="btn btn-default" type="button">Go!</button>
+          </span>
+		  </div>		  
+        </div>
+
+		
+      </div>
+	  
+      <div class="collapse navbar-collapse" id="navbar-collapse-1">
         <!-- Nav bar content -->
-        <ul class="nav navbar-nav navbar-right">
-		<!--
+        <ul class="nav navbar-nav">
           <li>
-            <a href="#" id="login" data-toggle="modal" data-target="#myModal">Log In</a>
+            <a href="#">dsadsa</a>
           </li>
-          <li>
-            <a href="#signupForm">
-              <button class="btn-primary btn-lg" type="button" id="signup">Sign Up</button>
-            </a>
-          </li>-->
         </ul>
       </div>
       <!-- /.navbar-collapse -->
     </nav>
   </div>
   <!--End of Navbar-->
-  
-    <!--Navbar 2-->
-  <div class="row">
+  <!--Navbar 2-->
+  <div class="row" id="second-row">
     <nav class="navbar navbar-default" id="subnav">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-      <!-- Responsive nav bar settings -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
+      <div class="container" id="second-container">
+        <div class="navbar-header">
+          <!-- Responsive nav bar settings -->
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-2">
+          <span class="sr-only">Toggle navigation</span> 
+		  <span class="icon-bar"></span>
 		<span class="icon-bar"></span>
 		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-      </button> 
-      <p>This is the second bar</p>
-	  
-	  </div>
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <!-- Nav bar content -->
-        <ul class="nav navbar-nav navbar-right" id="subnavbar">
-		<!--
-          <li>
-            <a href="#" id="login" data-toggle="modal" data-target="#myModal">Log In</a>
-          </li>
-          <li>
-            <a href="#signupForm">
-              <button class="btn-primary btn-lg" type="button" id="signup">Sign Up</button>
-            </a>
-          </li>-->
-        </ul>
+		  </button>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-collapse-2">
+          <!-- Nav bar content -->
+          <ul class="nav navbar-nav" id="subnavbar">
+            <li>
+              <a href="#">Dashboard</a>
+            </li>
+            <li>
+              <a href="#">Graphs</a>
+            </li>
+            <li>
+              <a href="#">Unpaid Bills</a>
+            </li>
+            <li>
+              <a href="#">Recent</a>
+            </li>
+            <li>
+              <a href="#">Templates</a>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+              aria-expanded="false">Dropdown</a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="#">Action</a>
+                </li>
+                <li>
+                  <a href="#">Another action</a>
+                </li>
+                <li>
+                  <a href="#">Something else here</a>
+                </li>
+                <li class="dropdown-header">Nav header</li>
+                <li>
+                  <a href="#">Separated link</a>
+                </li>
+                <li>
+                  <a href="#">One more separated link</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
       </div>
-      <!-- /.navbar-collapse -->
     </nav>
   </div>
   <!--End of Navbar 2-->
-
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
   <!-- Include all compiled plugins (below), or include individual files as needed -->
    
   <script src="js/bootstrap.min.js"></script> 
-  <script src="js/validator.min.js"></script></div></body>
+  <script src="js/validator.min.js"></script></body>
 </html>
