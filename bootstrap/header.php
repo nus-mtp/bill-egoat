65436b390b8a8bf6b408ff0ab35adc43d9285ca3
daryl2158 <daryl2158@hotmail.com>
2015-11-05 15:43:19 +0800

    Finished header

--- bootstrap/header.php
+++ bootstrap/header.php
@@ -1,151 +1,119 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta name="generator"
-    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
-    <link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png" />
-    <link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-icon-114x114.png" />
-    <link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png" />
-    <link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png" />
-    <link rel="icon" type="image/png" sizes="192x192" href="favicons/android-icon-192x192.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
-    <link rel="icon" href="favicons/favicon.ico" type="image/x-icon" />
-    <link rel="shortcut icon" href="favicons/favicon.ico" type="image/x-icon" />
-    <link rel="manifest" href="favicons/manifest.json" />
-    <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png" />
-    <meta name="theme-color" content="#ffffff" />
-    <meta name="description"
-    content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
-    <!-- Open Graph data -->
-    <meta property="og:title" content="Bill.eGoat" />
-    <meta property="og:type" content="article" />
-    <meta property="og:url" content="https://www.billegoat.gq/" />
-    <meta property="og:image" content="https://www.billegoat.gq/images/landing/logo.png" />
-    <meta property="og:description"
-    content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
-    <link href='https://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css' />
-    <title>Bill.eGoat</title>
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" />
-  </head>
-  <body>
-  <link href="css/header.css" rel="stylesheet" />
-  <!--Navbar-->
-  <div class="row" id="first-row">
-    <nav class="navbar navbar-default">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="container">
-        <div class="navbar-header">
-        <!-- Responsive nav bar settings -->
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-		  <span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-        </button> 
-
-        <a class="navbar-brand" href="https://www.billegoat.gq">
-          <img class="img-responsive" style="max-height:40px" src="images/landing/logo.png" alt="Logo" />
-        </a></div>
-		
-        <div class="col-md-4" id="searchbar">
-          <div class="input-group">
-          <input type="text" class="form-control" placeholder="Search for Bills" /> 
-          <span class="input-group-btn">
-            <button class="btn btn-default" type="button">Go!</button>
-          </span>
-		  </div>		  
-        </div>
-
-		
-      </div>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="apple-touch-icon" sizes="57x57" href="favicons/apple-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="60x60" href="favicons/apple-icon-60x60.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="favicons/apple-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="76x76" href="favicons/apple-icon-76x76.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="favicons/apple-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="120x120" href="favicons/apple-icon-120x120.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="favicons/apple-icon-144x144.png" />
+		<link rel="apple-touch-icon" sizes="152x152" href="favicons/apple-icon-152x152.png" />
+		<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-icon-180x180.png" />
+		<link rel="icon" type="image/png" sizes="192x192" href="favicons/android-icon-192x192.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+		<link rel="icon" href="favicons/favicon.ico" type="image/x-icon" />
+		<link rel="shortcut icon" href="favicons/favicon.ico" type="image/x-icon" />
+		<link rel="manifest" href="favicons/manifest.json" />
+		<meta name="msapplication-TileColor" content="#ffffff" />
+		<meta name="msapplication-TileImage" content="favicons/ms-icon-144x144.png" />
+		<meta name="theme-color" content="#ffffff" />
+		<meta name="description"
+		content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
+		<!-- Open Graph data -->
+		<meta property="og:title" content="Bill.eGoat" />
+		<meta property="og:type" content="article" />
+		<meta property="og:url" content="https://www.billegoat.gq/" />
+		<meta property="og:image" content="https://www.billegoat.gq/images/landing/logo.png" />
+		<meta property="og:description"
+		content="Bill.eGoat is an automatic bill sorting, tracking and analysis system for individuals or organisations." />
+		<link href='https://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css' />
+		<title>Bill.eGoat</title>
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" />
+		<link href="css/header.css" rel="stylesheet" />
 	  
-      <div class="collapse navbar-collapse" id="navbar-collapse-1">
-        <!-- Nav bar content -->
-        <ul class="nav navbar-nav">
-          <li>
-            <a href="#">dsadsa</a>
-          </li>
-        </ul>
-      </div>
-      <!-- /.navbar-collapse -->
-    </nav>
-  </div>
-  <!--End of Navbar-->
-  <!--Navbar 2-->
-  <div class="row" id="second-row">
-    <nav class="navbar navbar-default" id="subnav">
-      <div class="container" id="second-container">
-        <div class="navbar-header">
-          <!-- Responsive nav bar settings -->
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-2">
-          <span class="sr-only">Toggle navigation</span> 
-		  <span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		<span class="icon-bar"></span>
-		  </button>
-        </div>
-        <div class="collapse navbar-collapse" id="navbar-collapse-2">
-          <!-- Nav bar content -->
-          <ul class="nav navbar-nav" id="subnavbar">
-            <li>
-              <a href="#">Dashboard</a>
-            </li>
-            <li>
-              <a href="#">Graphs</a>
-            </li>
-            <li>
-              <a href="#">Unpaid Bills</a>
-            </li>
-            <li>
-              <a href="#">Recent</a>
-            </li>
-            <li>
-              <a href="#">Templates</a>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-              aria-expanded="false">Dropdown</a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a href="#">Action</a>
-                </li>
-                <li>
-                  <a href="#">Another action</a>
-                </li>
-                <li>
-                  <a href="#">Something else here</a>
-                </li>
-                <li class="dropdown-header">Nav header</li>
-                <li>
-                  <a href="#">Separated link</a>
-                </li>
-                <li>
-                  <a href="#">One more separated link</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-      </div>
-    </nav>
-  </div>
-  <!--End of Navbar 2-->
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-   
-  <script src="js/bootstrap.min.js"></script> 
-  <script src="js/validator.min.js"></script></body>
+		<!--Header Body-->
+		<nav class="navbar navbar-default navbar-static-top" role="navigation" id="navbar-1">
+		
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					
+					<a class="navbar-brand" href="#"> 
+						<img class="img-responsive" style="max-height:37px" src="images/landing/logo.png" alt="Logo" />
+					</a>
+				</div>
+
+				<!-- Main Navbar-->
+				<div class="collapse navbar-collapse" id="navbar-collapse-1">
+					<div class="col-sm-4 col-md-4">
+						<form class="input-form" role="search" id="searchbar">
+							<div class="input-group">
+								<input type="text" class="form-control" placeholder="Search for bills">
+								<div class="input-group-btn">
+									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+								</div>
+							</div>
+						</form>
+					</div>
+				
+					<ul class="nav navbar-nav">
+						<li>
+							<a href="#">
+								<button class="btn-primary btn-lg" type="button" id="Add"><span class="glyphicon glyphicon-plus"></span>&nbsp Add Bill</button>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<button class="btn btn-default btn-lg btn-link" style="font-size:18px;">
+									<span class="glyphicon glyphicon-bell"></span>
+								</button>
+								<span class="badge badge-notify">3</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>        
+
+		<!-- subnav here -->
+		<div class="navbar navbar-default" role="navigation" id="navbar-2">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-2">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="navbar-collapse-2">
+					<ul class="nav navbar-nav navbar" id="navbar-2-links">
+						<li><a href="#">Dashboard</a></li>
+						<li><a href="#">Graphs</a></li>
+						<li><a href="#">Unpaid Bills</a></li>
+						<li><a href="#">Recent Bills</a></li>
+						<li><a href="#">Templates</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	  
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script> 
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+	   
+		<script src="js/bootstrap.min.js"></script> 
+		<script src="js/validator.min.js"></script>
+	</head>
 </html>
