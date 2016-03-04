8c040937abc191f9fac00e754d3626f115e55dc6
daryl2158 <daryl2158@hotmail.com>
2016-03-04 14:14:02 +0800

    Updated Manual add/update models
    -Able to view file preview, auto-load database values
    -Revision no auto-updates

--- application/controllers/MAddBill.php
+++ application/controllers/MAddBill.php
@@ -2,35 +2,50 @@
 
 class MAddBill extends CI_Controller {
 
- public function index()
- {
-      $data['title'] = "Manually Add Bill";
-	  $data['headline'] = "Add a Bill Manually";
-	  $data['include'] = 'maddbill_view';
-	  $this->load->vars($data);
-	  $this->load->view('template');
- }
- 
- public function addManualBill()
- {
-    // Load the model
-    $this->load->model('Maddbill_model','',TRUE);
-	$imageName = $this->Maddbill_model->upload();
-	$this->Maddbill_model->insert_bills_table($imageName);
-	//$this->Maddbill_model->insert_bill_amts_table();
-	//$this->Maddbill_model->insert_bill_tags_table();
-	
-   
-	redirect('graph','refresh');
-}
-    
-public function updateBill()
-{
-    $this->load->model('Maddbill_model','',TRUE);
-    $this->Maddbill_model->update_bills_table($this->input->post('billID'));
-    redirect('graph','refresh');
-}
-
+	 public function index()
+	 {
+		$data['title'] = "Manually Add Bill";
+		$data['headline'] = "Add a Bill Manually";
+		$data['include'] = 'maddbill_view';
+		$this->load->vars($data);
+		$this->load->library('unit_test');
+		$this->unit->active(TRUE);
+		$this->load->view('template');
+	 }
+	 
+	 public function addManualBill()
+	 {
+		// Load the model
+		$this->load->model('Maddbill_model','',TRUE);
+		
+		// Upload file prompt and actual upload
+		$imageName = $this->Maddbill_model->upload();
+		
+		// Post other text fields to DB
+		$this->Maddbill_model->insert_bills_table($imageName);
+		
+		//Inputs to be added
+		//$this->Maddbill_model->insert_bill_amts_table();
+		//$this->Maddbill_model->insert_bill_tags_table();
+	   
+	   // Load bill summary page
+		redirect('graph','refresh');
+	}
+		
+	public function updateBill()
+	{
+		// Load the model
+		$this->load->model('Maddbill_model','',TRUE);
+		
+		// Upload replacement file
+		$imageName = $this->Maddbill_model->upload();
+		
+		// Post fields to DB
+		$this->Maddbill_model->update_bills_table($this->input->post('billID'), $imageName);
+		
+		// Load bill summary page
+		redirect('graph','refresh');
+	}
 }
 
 ?>
--- application/models/Maddbill_model.php
+++ application/models/Maddbill_model.php
@@ -1,6 +1,6 @@
 <?php
-class Maddbill_model extends CI_Model {
-
+class Maddbill_model extends CI_Model 
+{
 	function __construct()
 	{
 		parent::__construct();
@@ -9,46 +9,84 @@ class Maddbill_model extends CI_Model {
 		$this->billdb = $CI->load->database('billdb', TRUE);
 	}
 	
+	/*Inserts form fields into DB billdb.bills
+	** @author Daryl Lim
+	** @Parameter: image name to be posted to DB
+	** @Output: Row ID/billID of inserted path
+	*/
 	public function insert_bills_table($imageName)
-	
 	{	
+		//Set default amount to 0 instead of NULL if left empty, for graphing purposes
 		if ($this->input->post('totalAmt')==NULL){
 			$amt=0;
-		}else{
+		}
+		else
+		{
 			$amt= $this->input->post('totalAmt');
 		}
 		
+		//Set filename to NULL instead of blank if left empty, to avoid duplicate empty str filepaths
 		if ($imageName==NULL){
 			$file=NULL;
-		}else{
+		}
+		else
+		{
+			//Append parent directory to image name
 			$file="images/".$imageName;
 		}
 		
+		//Prepare array for posting to DB
 		$data = array(
 			'billFilePath' => $file,
-			'submittedTimestamp' => date("Y-m-d h:m:s",time()),
-			'revisionNo' => 1,
-			'templateID' => 1,
+			'submittedTimestamp' => date("Y-m-d h:m:s",time()), //Automatically post current server time
+			'revisionNo' => 1, //Default revision upon creation
+			'templateID' => 0, //Default for manually entered bill with no template
 			'billSentDate' => $this->input->post('billSentDate'),
 			'billDueDate' => $this->input->post('billDueDate'),
 			'totalAmt' => $amt,
-			'billIsComplete' => FALSE,
-			'billIsVerified' => TRUE,
-			'billIsCopy' => FALSE,
-			'billCompleteDateTime' => NULL,	
+			'billIsComplete' => FALSE, //Default, bill is not complete
+			'billIsVerified' => TRUE, //Default, bill is 'human-verified' as it is manually entered.
+			'billIsCopy' => FALSE, //Default, as bill is original copy
+			'billCompleteDateTime' => NULL,	//Default, bill is not complete and so has no time complete
 		);
-
+		
+		//Post form values to DB
 		$this->billdb->insert('bills', $data);
 		return $id = $this->db->insert_id();
 	}
     
-    public function update_bills_table($billID) 
+    public function update_bills_table($billID, $imageName) 
     {
-        $data = array(
-            'billSentDate' => $this->input->post('billSentDate'),
+		//Set default amount to 0 instead of NULL if left empty, for graphing purposes
+		if ($this->input->post('totalAmt')==NULL){
+			$amt=0;
+		}
+		else
+		{
+			$amt= $this->input->post('totalAmt');
+		}
+		
+		//If no file uploaded, do not replace current file
+		if ($imageName==NULL){
+			$data = array();
+		}
+		else
+		{
+			//Append parent directory to new image name
+			$file="images/".$imageName;
+			$data = array('billFilePath' => $file);
+		}
+		
+		//Append text fields to array for posting to DB
+		
+		$new_data = array(
+			'revisionNo' => $this->input->post('revisionNo'), //Default revision upon creation
+			'billSentDate' => $this->input->post('billSentDate'),
 			'billDueDate' => $this->input->post('billDueDate'),
-			'totalAmt' => $this->input->post('amt'),
-        );
+			'totalAmt' => $amt,
+		);
+		
+		$data = array_merge($data, $new_data);
             
         $this->billdb->where('billID',$billID);
         return $this->billdb->update('bills',$data);
@@ -82,29 +120,32 @@ class Maddbill_model extends CI_Model {
 		
 	public function upload()
 	{
-  if(isset($_FILES['image'])){
-      $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size = $_FILES['image']['size'];
-      $file_tmp = $_FILES['image']['tmp_name'];
-      $file_type = $_FILES['image']['type'];
-	  $tmp = explode('.',$_FILES['image']['name']);
-      $file_ext=strtolower(end($tmp));
+		if(isset($_FILES['image']))
+		{
+			$errors= array();
+			$file_name = $_FILES['image']['name'];
+			$file_size = $_FILES['image']['size'];
+			$file_tmp = $_FILES['image']['tmp_name'];
+			$file_type = $_FILES['image']['type'];
+			$tmp = explode('.',$_FILES['image']['name']);
+			$file_ext=strtolower(end($tmp));
       
       
-      if($file_size > 2097152) {
-         $errors[]='File size must be less than 2 MB';
-      }
+			if($file_size > 2097152) 
+			{
+				$errors[]='File size must be less than 2 MB';
+			}
       
-      if((empty($errors)==true)&&(($_FILES['image']['name'])!=NULL)) {
-		 $uniqueName = uniqid().".".$file_ext;
-         move_uploaded_file($file_tmp, "images/".$uniqueName);  
-		return $uniqueName;
-      }else{
-		  return NULL;
-      }
-   }
-	
+			if((empty($errors)==true)&&(($_FILES['image']['name'])!=NULL)) 
+			{
+				$uniqueName = uniqid().".".$file_ext;
+				move_uploaded_file($file_tmp, "images/".$uniqueName);  
+				return $uniqueName;
+			}
+			else
+			{
+				return NULL;
+			}
+		}
 	}
-	
 }
