6c01f2bbd04c30d6ae5b8224ace3bbd8573130eb
yhannq <yhann.q@gmail.com>
2016-03-17 23:33:26 +0800

    authorship indication

--- application/views/testGraph.php
+++ application/views/testGraph.php
@@ -1,4 +1,8 @@
-
+<!--
+/* View for getting and displaying bill data
+** @author Qiu Yunhan
+*/
+-->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
