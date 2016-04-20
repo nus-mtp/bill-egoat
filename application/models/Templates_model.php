3f9d7e4777f02cfbe5c9ca943bda62045ee69c46
Justin Doan <soundaholic92@gmail.com>
2016-04-20 11:17:12 +0800

    Improved OCR data formatting
    All common date formats are now supported, regardless of date/month
    order, and whether month is written in full or short form. Any commas or
    full stops are also removed.
    
    All non-numerical characters are automatically removed from due amount.

--- application/models/Templates_model.php
+++ application/models/Templates_model.php
@@ -1,7 +1,9 @@
 <?php
 
 //@Author James Ho, refactored by Tan Tack Poh
+
 /*Bill Template System
+
 */   
 class Templates_model extends CI_Model {
 
@@ -31,38 +33,71 @@ class Templates_model extends CI_Model {
     // @Author James Ho
     // Write into SQL table the coordinates data of parts of template (table is templatedb -> datafields)
     public function insert_datafields_table($templateCoords,$templateID)
+
     {
+
         
+
         $dataLogo = array(
+
             'templateID' => $templateID,
+
             'dataFieldLabel' => "logo",
+
             'coordinateLabelX' => $templateCoords[0][0],
+
             'coordinateLabelY' => $templateCoords[0][1],
+
             'coordinateLabelX2' => $templateCoords[0][2],
+
             'coordinateLabelY2' => $templateCoords[0][3],
+
         );
+
         
+
         $dataDueDate = array(
+
             'templateID' => $templateID,
+
             'dataFieldLabel' => "duedate",
+
             'coordinateLabelX' => $templateCoords[1][0],
+
             'coordinateLabelY' => $templateCoords[1][1],
+
             'coordinateLabelX2' => $templateCoords[1][2],
+
             'coordinateLabelY2' => $templateCoords[1][3],
+
         );
+
         
+
         $dataAmount = array(
+
             'templateID' => $templateID,
+
             'dataFieldLabel' => "amount",
+
             'coordinateLabelX' => $templateCoords[2][0],
+
             'coordinateLabelY' => $templateCoords[2][1],
+
             'coordinateLabelX2' => $templateCoords[2][2],
+
             'coordinateLabelY2' => $templateCoords[2][3],
+
         );
+
         
+
         $this->templatedb->insert('datafields', $dataLogo);
+
         $this->templatedb->insert('datafields', $dataDueDate);
+
         $this->templatedb->insert('datafields', $dataAmount);
+
     }
 
     // @Author Tan Tack Poh
@@ -74,7 +109,7 @@ class Templates_model extends CI_Model {
         $LogoDBFilesDirectory = "images/logos_DB/";
         list($width, $height) = getimagesize($billFilePath);
 
-        $query = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = " . $templateID ." AND dataFieldLabel = 'logo'");
+        $query = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = '" . $templateID ."' AND dataFieldLabel = 'logo'");
         $row = $query->row(0);
         $x1 = $row->coordinateLabelX;
         $y1 = $row->coordinateLabelY;
@@ -82,8 +117,11 @@ class Templates_model extends CI_Model {
         $y2 = $row->coordinateLabelY2 - $row->coordinateLabelY;
 
         $x1 = $x1 * $width;
+
         $y1 = $y1 * $height;
+
         $x2 = $x2 * $width;
+
         $y2 = $y2 * $height;
 
         //cropping the image using the coordinate data
@@ -93,8 +131,11 @@ class Templates_model extends CI_Model {
         imagejpeg($thumb_im, $LogoDBFilesDirectory . $templateID . '.jpg', 100);
 
         $dataLogoFilePath = array(
+
             'templateID' => $templateID,
+
             'logodbImgPath' => $LogoDBFilesDirectory . $templateID . '.jpg',
+
         );
 
         $this->templatedb->insert('logo', $dataLogoFilePath);
@@ -115,10 +156,16 @@ class Templates_model extends CI_Model {
     }
     
     //@Author and refactored by Tan Tack Poh
+
     /*Use Template ID to obtain already existing template details, then perform logo recognition
+
       */   
+
     public function logoRecognition ($billID, $templateID)      
+
     {
+
+
 
         // Bill Image
         $this->billdb->select('billFilePath');
@@ -138,12 +185,15 @@ class Templates_model extends CI_Model {
         $detectionInputTrainingImgName = "trainingImage.jpg"; // Hardcoded training input as OpenCV doesn't handle arguments
 
         // Parameters for the recognition result image's directory and file name, and the recognition algorithm file's too
+
         $detectionFileDirectory = "application/views/OpenCV_Main/";
+
         $detectionFileName = "logo_recognition.py";  
+
         $resultFileImgName = "recognitionResult.jpg"; // Hardcoded output as OpenCV doesn't handle arguments
 
         // Get correct coordinate for logo, scaled down to between 0 to 1
-        $query = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = " . $templateID ." AND dataFieldLabel = 'logo'");
+        $query = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = '" . $templateID ."' AND dataFieldLabel = 'logo'");
         $row = $query->row(0);
         $x1 = $row->coordinateLabelX;
         $y1 = $row->coordinateLabelY;
@@ -152,8 +202,11 @@ class Templates_model extends CI_Model {
 
         // Scale up coordinates from 0 to 1 to actual coordinates
         $x1 = $x1 * $width;
+
         $y1 = $y1 * $height;
+
         $x2 = $x2 * $width;
+
         $y2 = $y2 * $height;
 
         //cropping the image using the coordinate data
@@ -165,41 +218,78 @@ class Templates_model extends CI_Model {
        
         $logo_DB = scandir($LogoDBFilesDirectory);
 
+
+
         foreach($logo_DB as &$logoDBImgName){   
 
+
+
             // copy current database logo to detection folder as different name for py file to process
+
             $command = escapeshellcmd('cp ' . $LogoDBFilesDirectory . $logoDBImgName . " " . $detectionInputDirectory . $detectionInputTrainingImgName);
+
             shell_exec($command);
+
+
 
             // Run Detection
+
             $command = escapeshellcmd('python ' . $detectionFileDirectory . $detectionFileName);
+
             shell_exec($command);
+
+
 
             // remove the image once the check is complete.
+
             $command = escapeshellcmd('rm -f ' . $detectionInputDirectory . $resultFileImgName);
+
             shell_exec($command);
 
+
+
             // delete current database logo from detection folder when done
+
             $command = escapeshellcmd('rm -f ' . $detectionInputDirectory . $detectionInputTrainingImgName);
+
             shell_exec($command);
+
+
 
         }
 
+
+
         // delete current query logo from detection folder when done
+
         $command = escapeshellcmd('rm -f ' . $detectionInputDirectory . $detectionInputImgName);
+
         shell_exec($command); 
 
+
+
     }
+
     
+
     //@Author Justin Doan, refactored/modified for security by Tan Tack Poh
+
     /*OCR processing, including reading the image, retrieving the coordinate data from the database,
+
       cropping out that part of the image and run OCR on it
+
       */   
+
     public function ocr($billID, $template)        
+
     {
+
         $amountImgFileDirectory = "images/detection_result/";
+
         $dueDateImgFileDirectory = "images/detection_result/";
+
         $amountImgFileName = "croppedAmt.jpg";
+
         $dueDateImgFileName = "croppedDueDate.jpg";
 
         //connect to mysql and getting the coordinate data
@@ -213,7 +303,7 @@ class Templates_model extends CI_Model {
 
         list($width, $height) = getimagesize($ini_filename);
 
-        $query2 = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = " . $template ." AND dataFieldLabel = 'amount'");
+        $query2 = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = '" . $template ."' AND dataFieldLabel = 'amount'");
         $row = $query2->row(0);
         $x1 = $row->coordinateLabelX;
         $y1 = $row->coordinateLabelY;
@@ -222,8 +312,11 @@ class Templates_model extends CI_Model {
 
         // Scale Up coordinates
         $x1 = $x1 * $width;
+
         $y1 = $y1 * $height;
+
         $x2 = $x2 * $width;
+
         $y2 = $y2 * $height;
 
         //cropping the image using the coordinate data
@@ -236,8 +329,9 @@ class Templates_model extends CI_Model {
         $tesseract = new TesseractOCR($amountImgFileDirectory . $amountImgFileName);
         $tesseract->setLanguage('eng');
         $amount = $tesseract->recognize();
+		$amount = preg_replace("/[^0-9,.]/", "", $amount);
 
-        $query3 = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = " . $template ." AND dataFieldLabel = 'duedate'");
+        $query3 = $this->templatedb->query("SELECT coordinateLabelX, coordinateLabelY, coordinateLabelX2, coordinateLabelY2 FROM datafields WHERE templateID = '" . $template ."' AND dataFieldLabel = 'duedate'");
         $row = $query3->row(0);
         $x1 = $row->coordinateLabelX;
         $y1 = $row->coordinateLabelY;
@@ -246,8 +340,11 @@ class Templates_model extends CI_Model {
 
         // Scale Up coordinates
         $x1 = $x1 * $width;
+
         $y1 = $y1 * $height;
+
         $x2 = $x2 * $width;
+
         $y2 = $y2 * $height;
         
         //cropping the image using the coordinate data
@@ -266,42 +363,63 @@ class Templates_model extends CI_Model {
         $day = strtok($duedate, " ");
         $month = strtok(" ");
         $year = strtok(" ");
+        
+        str_replace(array(",", "."), "", $day);
+        str_replace(array(",", "."), "", $month);
+        str_replace(array(",", "."), "", $year);
+        
+        if (ctype_alpha($day)) {
+        	$temp = $day;
+        	$day = $month;
+        	$month = $temp;
+        }
 
         switch ($month) {
             case 'Jan':
+            case 'January':
                 $month = "01";
                 break;
             case 'Feb':
+            case 'February':
                 $month = "02";
                 break;
             case 'Mar':
+            case 'March':
                 $month = "03";
                 break;
             case 'Apr':
+            case 'April':
                 $month = "04";
                 break;
             case 'May':
                 $month = "05";
                 break;
             case 'Jun':
+            case 'June':
                 $month = "06";
                 break;
             case 'Jul':
+            case 'July':
                 $month = "07";
                 break;
             case 'Aug':
+            case 'August':
                 $month = "08";
                 break;
             case 'Sep':
+            case 'September':
                 $month = "09";
                 break;
             case 'Oct':
+            case 'October':
                 $month = "10";
                 break;
             case 'Nov':
+            case 'November':
                 $month = "11";
                 break;
             case 'Dec':
+            case 'December':
                 $month = "12";
                 break;
         }
@@ -313,12 +431,16 @@ class Templates_model extends CI_Model {
         $this->billdb->where('billID', $billID);
         $this->billdb->update('bills',$data);
 
-        // remove the cropped images once the check is complete.
+        /* remove the cropped images once the check is complete.
+
         $command = escapeshellcmd('rm -f ' . $amountImgFileDirectory . $amountImgFileName);
+
         shell_exec($command);
         $command = escapeshellcmd('rm -f ' . $dueDateImgFileDirectory . $dueDateImgFileName);
-        shell_exec($command);
 
+        shell_exec($command);
+		*/
+		
         return $ini_filename;
     }
     
